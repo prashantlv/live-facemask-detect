c8dd1ac8aac10b2cf5df88d9e7e74a5cc745bb99
playground-n7 <shukla.prashant689@gmail.com>
2020-08-23 20:13:58 +0530

    draw rec and predict

--- run.py
+++ run.py
@@ -17,4 +17,31 @@ while True:
     img = cv2.resize(im, (im.shape[1]//4 , im.shape[0]//4))
     faces = classifier.detectMultiScale(img)
 
+    for f in faces:
+        (x, y, w, h) = [v * size for v in f] #Scale the shapesize backup
+        #Save just the rectangle faces in SubRecFaces
+        face_img = im[y:y+h, x:x+w]
+        resized=cv2.resize(face_img,(150,150))
+        normalized=resized/255.0
+        reshaped=np.reshape(normalized,(1,150,150,3))
+        reshaped = np.vstack([reshaped])
+        result=model.predict(reshaped)
+        #print(result)
+        
+        label=np.argmax(result,axis=1)[0]
+      
+        cv2.rectangle(im,(x,y),(x+w,y+h),color_dict[label],2)
+        cv2.rectangle(im,(x,y-40),(x+w,y),color_dict[label],-1)
+        cv2.putText(im, labels_dict[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+        
     
+    cv2.imshow('LIVE',   im)
+    key = cv2.waitKey(10)
+     
+    if key == 27: #The Esc key
+        break
+
+webcam.release()
+
+# Close all started windows
+cv2.destroyAllWindows()
